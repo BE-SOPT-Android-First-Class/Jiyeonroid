6c05d05bff5c8411bd3486cbe7d49e60703f2303
jiyeon <dpswpfkgy@gamil.com>
2021-08-01 23:19:48 +0900

    [init] init file

--- app/src/main/java/org/sopt/week4/MyGlideApp.kt
+++ app/src/main/java/org/sopt/week4/MyGlideApp.kt
@@ -1,7 +1,7 @@
 package org.sopt.week4
 
-import com.bumptech.glide.annotation.GlideModule
-import com.bumptech.glide.module.AppGlideModule
-
-@GlideModule
-class MyGlideApp: AppGlideModule()
+//import com.bumptech.glide.annotation.GlideModule
+//import com.bumptech.glide.module.AppGlideModule
+//
+//@GlideModule
+//class MyGlideApp: AppGlideModule()
