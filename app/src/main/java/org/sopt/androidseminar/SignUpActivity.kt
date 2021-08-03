d75a23550c7d486b123f52995398f572e5f12f63
jiyeon <dpswpfkgy@gamil.com>
2021-08-03 22:15:22 +0900

    [modify] code review 반영

--- app/src/main/java/org/sopt/androidseminar/SignUpActivity.kt
+++ app/src/main/java/org/sopt/androidseminar/SignUpActivity.kt
@@ -1,70 +1,74 @@
 package org.sopt.androidseminar
 
+import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import org.sopt.androidseminar.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
-    private lateinit var binding : ActivitySignUpBinding
-    private val logTag = "logTag"
+
+    private lateinit var binding: ActivitySignUpBinding
+    private val tag = "SignUpActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        singUpButtonClickEvent()
+
+        signUpButtonClickEvent()
+    }
+
+    private fun signUpButtonClickEvent() {
+        binding.btnSignUp.setOnClickListener {
+            if (isEmptyBlank()) {
+                Toast.makeText(this, "빈 칸이 있는지 확인해주세요", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent(this, SignUpActivity::class.java)
+
+                intent.putExtra("githubId", binding.etGithubId.text.toString())
+                intent.putExtra("pwd", binding.etSignupPwd.text.toString())
+                intent.putExtra("name", binding.etName.text.toString())
+                setResult(Activity.RESULT_OK, intent)
+
+                finish()
+            }
+        }
+    }
+
+    private fun isEmptyBlank(): Boolean {
+        return binding.etGithubId.text.isNullOrBlank() || binding.etSignupPwd.text.isNullOrBlank() || binding.etName.text.isNullOrBlank()
     }
 
     override fun onStart() {
         super.onStart()
-        Log.d(logTag, "SignUpActivity - onStart")
+        Log.d(tag, "onStart")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.d(logTag, "SignUpActivity - onResume")
+        Log.d(tag, "onResume")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.d(logTag, "SignUpActivity - onPause")
+        Log.d(tag, "onPause")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.d(logTag, "SignUpActivity - onStop")
+        Log.d(tag, "onStop")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.d(logTag, "SignUpActivity - onDestroy")
+        Log.d(tag, "onDestroy")
     }
 
     override fun onRestart() {
         super.onRestart()
-        Log.d(logTag, "SignUpActivity - onRestart")
-    }
-
-    private fun singUpButtonClickEvent() {
-        binding.btnSignUp.setOnClickListener {
-            val userName = binding.etName.text
-            val usergithubId = binding.etGithubId.text
-            val userPwd = binding.etSignupPwd.text
-            if (userName.isNullOrBlank() || usergithubId.isNullOrBlank() || userPwd.isNullOrBlank()) {
-                Toast.makeText(this@SignUpActivity, "빈칸이 있는지 확인해주세요", Toast.LENGTH_SHORT).show()
-            } else {
-                val intent = Intent(this@SignUpActivity, HomeActivity::class.java)
-                val bundle = Bundle();
-                bundle.putString("userName", userName.toString())
-                bundle.putString("userPwd", userPwd.toString())
-                bundle.putString("usergithubId", usergithubId.toString())
-                intent.putExtras(bundle)
-                startActivity(intent)
-
-            }
-        }
+        Log.d(tag, "onRestart")
     }
 }
