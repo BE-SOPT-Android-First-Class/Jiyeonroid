9e63f03bc6c5301be15b862f6dd994513e08dacd
jiyeon <dpswpfkgy@gamil.com>
2021-08-12 22:54:53 +0900

    [modify] 이름 수정

--- app/src/main/java/org/sopt/androidseminar/data/FollowingUserInfo.kt
+++
@@ -1,7 +0,0 @@
-package org.sopt.androidseminar.data
-
-data class FollowingUserInfo(
-    val userName: String
-) {
-    val userImgSrc = "https://github.com/$userName.png"
-}
--- app/src/main/java/org/sopt/androidseminar/ui/MainActivity.kt
+++ app/src/main/java/org/sopt/androidseminar/ui/MainActivity.kt
@@ -1,8 +1,10 @@
 package org.sopt.androidseminar.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import org.sopt.androidseminar.data.RepoListInfo
 import org.sopt.androidseminar.databinding.ActivityHomeBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +16,49 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        moreButtonClickEvent()
+        setRepoList()
+    }
+
+    private fun moreButtonClickEvent() {
+        binding.btnHomeMore.setOnClickListener {
+            val intent = Intent(this, UserInfoActivity::class.java)
+//            ActivityResultContracts.StartActivityForResult()
+            startActivity(intent)
+        }
+    }
+
+    private fun setRepoList() {
+        val repoListAdapter = RepoListAdapter()
+        binding.rvHomeRepoList.adapter = repoListAdapter
+        repoListAdapter.setRepositoryList(
+            listOf(
+                RepoListInfo("레포이름", "레포설명", "레포언어"),
+                RepoListInfo("레포이름", "레포설명", "레포언어"),
+                RepoListInfo("레포이름", "레포설명", "레포언어"),
+                RepoListInfo("레포이름", "레포설명", "레포언어"),
+                RepoListInfo(
+                    "이ㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣ름",
+                    "설ㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹ명",
+                    "언ㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴ어"
+                )
+            )
+        )
+
+        for (i in 1..10) {
+            repoListAdapter.setRepositoryList(
+                listOf(
+                    RepoListInfo(
+                        "Repository Name".plus(i),
+                        "Repository Description".plus(i),
+                        "Repository Language".plus(i)
+                    )
+                )
+            )
+        }
+
+        repoListAdapter.notifyDataSetChanged()
     }
 
     override fun onStart() {
